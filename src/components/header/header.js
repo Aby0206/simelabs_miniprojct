6d81ee1a1e0a3cfb2e3375c9188eb1dde7e37bf1
Aby0206 <t.abyantonyd@gmail.com>
2023-08-22 16:42:57 +0530

    Header css updated

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './header.css';
 
 function Header() {
   return (
     <header>
       <nav>
-        <ul>
+        <ul className='horizontal-list'>
           <li><a href="/">Home</a></li>
           <li><a href="/add">Add Employee</a></li>
           <li><a href="/listen">Employee Listener</a></li>
